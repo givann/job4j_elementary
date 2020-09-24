430063581f2b56df83dbcfc7cf8004e7727608d2
anton <zas@see.ru>
2020-09-24 11:19:43 -0400

    Greeting

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like java ";
+        System.out.println(idea);
+
+        idea = idea + "But I am a newbie ";
+        System.out.println(idea);
+
+        int year = 2020;
+        idea += year;
+        System.out.println(idea);
+
+    }
+}
