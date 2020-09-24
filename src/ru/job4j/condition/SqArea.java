adf3d4d314f048736f6b07b5c24ccc7840086143
anton <zas@see.ru>
2020-09-24 11:04:10 -0400

    Point

---
+++ src/ru/job4j/condition/SqArea.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class SqArea {
+    public static double square(int p, int k) {
+        int h = p / (2 * (k + 1));
+        int l = h * k;
+        double rsl = h *l;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        double result1 = SqArea.square(4, 1);
+        System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
+    }
+}
