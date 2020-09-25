38e054aea9dca036356629287f752186494694bf
anton <zas@see.ru>
2020-09-24 23:06:48 -0400

    SqAreaTest

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -11,5 +11,7 @@ public class SqArea {
     public static void main(String[] args) {
         double result1 = SqArea.square(4, 1);
         System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
+
+        System.out.println(SqArea.square(10, 3));
     }
 }
