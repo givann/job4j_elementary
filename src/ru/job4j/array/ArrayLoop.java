77838fcbe8dff423adb03333474e79bf4290de3b
anton <zas@see.ru>
2020-09-27 00:15:48 -0400

    ArrayLoop

---
+++ src/ru/job4j/array/ArrayLoop.java
@@ -0,0 +1,12 @@
+package ru.job4j.array;
+
+public class ArrayLoop {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i * 2 + 3;
+            System.out.println(arr[i]);
+
+        }
+    }
+}
