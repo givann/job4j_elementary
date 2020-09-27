0e54cdfe5a700accf648cebcd6e940634f42c56d
anton <zas@see.ru>
2020-09-27 00:12:06 -0400

    ArraysNames

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,19 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+
+
+        String[] names = new String[4];
+
+        names[0] = "Ivan Ivanov";
+        names[1] = "Ivan Petrov";
+        names[2] = "Ivan Sodorov";
+        names[3] = "Ivan Beglov";
+
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
