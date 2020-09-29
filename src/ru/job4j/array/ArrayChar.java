542aba882fe8a252e97060a71de27798e3456d6a
anton <zas@see.ru>
2020-09-28 21:21:38 -0400

    square

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -3,19 +3,17 @@ package ru.job4j.array;
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
-        for (int i = 0; i < word.length; i++) {
-            for (int j = 0; j < pref.length; j++) {
-                if (word[j] != pref[j]) {
-                    return false;
-                }
-
+        for (int j = 0; j < pref.length; j++) {
+            if (word[j] != pref[j]) {
+                return false;
             }
+
         }
         return result;
     }
 
     public static void main(String[] args) {
-        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] word = {'H', 'e', 'l', 'l', 'у'};
         char[] pref = {'H', 'e', 'l', 'l', 'o'};
         System.out.println(startsWith(word, pref));
     }
