4d6d07def417995173aa6d98ebd1c0c85acacc94
anton <zas@see.ru>
2020-09-28 21:44:58 -0400

    square

---
+++ src/ru/job4j/array/EndsWith.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = true;
+        for (int i = post.length - 1, j = word.length - 1; i > 0;
+             i--, j--) {
+            if (post[i] != word[j]) {
+
+                return false;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'H', 'e', 'l', 'l', 'i'};
+
+        System.out.println(endsWith(word, post));
+    }
+}
