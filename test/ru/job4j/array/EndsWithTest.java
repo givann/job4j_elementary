4d514b823066ea24cd3e995125687e4c4400b6d5
anton <zas@see.ru>
2020-09-28 21:47:18 -0400

    EndsWith

--- test/ru/job4j/array/EndsWithTest.java
+++ test/ru/job4j/array/EndsWithTest.java
@@ -18,7 +18,7 @@ public class EndsWithTest {
     @Test
     public void whenNotEndWithPrefixThenFalse() {
         char[] word = {'H', 'e', 'l', 'l', 'o'};
-        char[] post = {'l', 'o'};
+        char[] post = {'L', 'o'};
         boolean result = EndsWith.endsWith(word, post);
         assertThat(result, is(false));
     }
