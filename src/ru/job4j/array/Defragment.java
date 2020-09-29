411d70fcccaa5e8110a3949117bb2b707583afa2
anton <zas@see.ru>
2020-09-29 01:22:49 -0400

    Defragment

---
+++ src/ru/job4j/array/Defragment.java
@@ -0,0 +1,35 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                int point = index;
+                for (int i = index; i < array.length; i++) {
+                    if (array[i] != null) {
+                        String tmp = array[i];
+                        array[i] = array[point];
+                        array[point] = tmp;
+                        break;
+                    }
+
+                }
+
+
+                /* указатель на null ячейку. */
+                /* переместить первую не null ячейку. Нужен цикл. */
+            }
+            System.out.print(array[index] + " ");
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}
